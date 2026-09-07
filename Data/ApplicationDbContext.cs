0a44381b659cb8cee31ec69a37ecdf194cf91858
ms-elliott <ms.elliott55@gmail.com>
2026-09-07 21:40:28 +0900

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){ }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                    new Category { Id = 1, Name = "Action" },
+                    new Category { Id = 2, Name = "SciFi" },
+                    new Category { Id = 3, Name = "History" }
+                );
+        }
+    }
+}
